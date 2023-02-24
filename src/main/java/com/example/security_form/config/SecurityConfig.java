c99ad774cd96af72936ce4eff2dc415d9d57291e
romanova-nat <100118618+romanova-nat@users.noreply.github.com>
2023-02-24 09:30:18 -0800

    Проблема с БД

--- src/main/java/com/example/security_form/config/SecurityConfig.java
+++ src/main/java/com/example/security_form/config/SecurityConfig.java
@@ -1,25 +1,25 @@
 package com.example.security_form.config;
 
-import com.example.security_form.repository.PersonRepository;
 import com.example.security_form.service.PersonDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.authentication.configuration.EnableGlobalAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalAuthentication
 public class SecurityConfig {
+
     private final PersonDetailService personDetailService;
 
     @Autowired
@@ -29,11 +29,12 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests().requestMatchers("/login").permitAll()
+        http.authorizeHttpRequests()
+                .requestMatchers("/login").permitAll()
                 .anyRequest().authenticated()
+                .and()
+                .formLogin().permitAll()
+                .defaultSuccessUrl("/show")
                 .and()
                 .logout()
                 .logoutUrl("/logout")
@@ -41,9 +42,16 @@ public class SecurityConfig {
         return http.build();
     }
 
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailService)
+                .passwordEncoder(getPasswordEncoder());
+    }
+
+
     //кодировка пароля
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
 }
